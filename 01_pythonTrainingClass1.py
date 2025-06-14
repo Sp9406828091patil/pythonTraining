8eed71f8fe3d2b71e27a160d60ef142565877501
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2025-06-14 08:26:43 +0530

    added syntax, indentation, variable

--- 01_pythonTrainingClass1.py
+++ 01_pythonTrainingClass1.py
@@ -1,1 +1,16 @@
-print("Hello jijaji")
+# define variable
+newVar1 = "Hello"
+print(newVar1)
+
+# python indentation
+if 5 > 2:
+    print("Five is greater then 2")
+    print("hello Aboli")
+
+# python variables
+firstVar = 5
+secondVar = "5"
+resumeFilepath = "C:\\Users\\HP\\Downloads\\AboliResume.pdf"
+print(firstVar, secondVar, resumeFilepath)
+print(type(firstVar), type(secondVar))
+print(firstVar*5, secondVar*5)
