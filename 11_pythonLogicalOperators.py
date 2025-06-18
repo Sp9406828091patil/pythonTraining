792f39244e2c051e029ebfd3ef4077733bc6d113
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2025-06-18 08:50:21 +0530

    added - logical operator

---
+++ 11_pythonLogicalOperators.py
@@ -0,0 +1,20 @@
+# python logical operators
+
+# 'and' operator
+x = 6
+print(x > 5 and x < 10)
+
+# 'or' operator
+x = 6
+print(x > 5 or x > 10)
+
+# 'not' operator
+x = 6
+print(not(x > 5 and x < 10))
+newString = "Aboli"
+print(newString != "Abolia")
+
+# python identity operator
+print(type(newString))
+print(newString is 'str')
+print(not(isinstance(newString, str)))
