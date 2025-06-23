01f369088227f92c5b24766b95bd159d7cda10b0
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2025-06-23 08:24:49 +0530

    added for loop

---
+++ 26_pythonForLoop.py
@@ -0,0 +1,10 @@
+# myString = "MyNameIsAboli"
+# for i in range(5, len(myString), 2):
+#     print(i)
+
+myList = [1, 2, 3, 4, 5]
+# for i in range(len(myList)):
+#     print(myList[i]**2)
+
+for i, j in enumerate(myList):
+    print(i, j)
